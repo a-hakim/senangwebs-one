0af1c3b614bb20035639a0af6aae8afd881b0514
A.Hakim Noor <a.hakim.solution@gmail.com>
2026-01-27 14:32:59 +0800

    fix: generate minified & remove old dists

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,70 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = {
-  entry: {
-    swo: './src/js/swo.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: 'SWO',
-      type: 'umd', 
-      export: 'default'
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: {
+      swo: './src/js/swo.js',
+      'swo.min': './src/js/swo.js', // Separate entry for minified version
     },
-    globalObject: 'this', 
-    publicPath: 'auto', // Let webpack determine the public path at runtime
-  },
-  // Disable code splitting to bundle everything into a single file
-  optimization: {
-    splitChunks: false,
-    runtimeChunk: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        name: 'SWO',
+        type: 'umd', 
+        export: 'default'
+      },
+      globalObject: 'this', 
+      publicPath: 'auto', 
+      clean: true, // Clean the output directory before emit
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map', // Enable source maps
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          include: /\.min\.js$/, // Only minify files ending in .min.js
+        }),
+        new CssMinimizerPlugin({
+          include: /\.min\.css$/, // Only minify files ending in .min.css
+        }),
+      ],
+      splitChunks: false,
+      runtimeChunk: false,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
           }
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.ttf$/,
+          type: 'asset/inline' 
         }
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.ttf$/,
-        type: 'asset/inline' // Inline fonts as base64 to avoid separate file requests
-      }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: ({ chunk }) => {
+          return `${chunk.name}.css`;
+        },
+      })
     ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'swo.css' 
-    })
-  ]
+  };
 };
